a5d948866f7d7ad1387c9eb17b17420ce8baea7e
FelipeMeloGomes <felipemelogomes36@gmail.com>
2025-12-02 15:00:25 -0300

    feat: add topo e rodape com bootstrap 5

--- rotas.php
+++ rotas.php
@@ -5,6 +5,6 @@ use Pecee\SimpleRouter\SimpleRouter as Router;
 Router::setDefaultNamespace('sistema\Controlador');
 
 Router::get(URL_SITE, 'SiteControlador@index');
-Router::get(URL_SITE . 'sobre', 'SiteControlador@sobre');
+Router::get(URL_SITE . 'sobre-nos', 'SiteControlador@sobre');
 
 Router::start();
